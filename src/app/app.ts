2256454a11a896b6a88662e9b01b8914c7080ff1
Ruma303 <matteoh8@hotmail.it>
2025-07-03 09:39:10 +0200

    init database

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angular-university';
+  protected title = 'Angular University';
 }
